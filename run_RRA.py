bbea4bc90f7527d1babfa55649f9e4e648ba5bb0
hrch3n <chr_3742@hotmail.com>
2022-11-30 12:35:22 -0500

    update package installation for better compatibility

--- run_RRA.py
+++ run_RRA.py
@@ -5,9 +5,10 @@ from os.path import join
 # from pipeline.segmentation.methods.installation.install_all_methods import *
 
 if __name__ == '__main__':
-	os.system('python image_download/download_HuBMAP_data.py')
-	os.system('python segmentation/run_segmentation.py')
-	os.system('python metric_calculation/run_metric_calculation.py')
-	os.system('bash result_analysis/reproduce_all_fig.sh')
+    os.system('pip install -r requirements.txt')
+    os.system('python image_download/download_HuBMAP_data.py')
+    os.system('python segmentation/run_segmentation.py')
+    os.system('python metric_calculation/run_metric_calculation.py')
+    os.system('bash result_analysis/reproduce_all_fig.sh')
 
 
